e7c80de807579eb50d9b5e8e54bec13d8746a6f9
DannyIdea <linhao997@qq.com>
2021-01-25 15:26:06 +0800

    parent pom adjust

---
+++ netty-base/src/main/java/org/idea/netty/framework/server/test/Test.java
@@ -0,0 +1,15 @@
+package org.idea.netty.framework.server.test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @Author linhao
+ * @Date created in 10:03 下午 2021/1/24
+ */
+public class Test {
+
+    public static void main(String[] args) {
+        List<String> test = new ArrayList<String>();
+    }
+}
